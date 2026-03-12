1662d88a2e34c4dfa268ff059233236477ef35b9
Ian De Bie <ian@debie.us>
2026-03-12 15:30:14 -0500

    added navigation

--- src/components/ChapterDisplay.tsx
+++ src/components/ChapterDisplay.tsx
@@ -2,9 +2,10 @@
 
 import { Chapter } from "@/models/models";
 import Link from "next/link";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import ChapterSelector from "./ChapterSelector";
 import TranslationSelector from "./TranslationSelector";
+import { BOOKS } from "@/models/metadata";
 
 interface ChapterDisplayProps {
     chapter: Chapter;
@@ -13,8 +14,47 @@ interface ChapterDisplayProps {
 }
 
 export default function ChapterDisplay({ chapter, bookId, translation }: ChapterDisplayProps) {
+    const currentBook = BOOKS[bookId];
+    const maxChapters = currentBook?.chapters || 1;
+
+    const hasPrev = chapter.chapterNumber > 1;
+    const hasNext = chapter.chapterNumber < maxChapters;
+
+    let prevLink = "";
+    if (chapter.chapterNumber > 1) {
+        prevLink = `/chapter/${bookId}/${chapter.chapterNumber - 1}?translation=${translation}`;
+    }
+
+    let nextLink = "";
+    if (chapter.chapterNumber < maxChapters) {
+        nextLink = `/chapter/${bookId}/${chapter.chapterNumber + 1}?translation=${translation}`;
+    }
+
     return (
-        <div className="flex min-h-screen flex-col bg-black text-white">
+        <div className="flex min-h-screen flex-col bg-black text-white relative">
+            {/* Navigation Buttons - Hidden on small screens or keep them? 
+                The requirement says "float above the text on left and right margin" 
+            */}
+            {hasPrev && (
+                <Link
+                    href={prevLink}
+                    className="fixed left-4 md:left-8 top-1/2 -translate-y-1/2 p-3 bg-black/50 border border-gray-700 rounded-full hover:bg-gray-800 transition-all z-20"
+                    title="Previous Chapter"
+                >
+                    <FaChevronLeft size={24} />
+                </Link>
+            )}
+
+            {hasNext && (
+                <Link
+                    href={nextLink}
+                    className="fixed right-4 md:right-8 top-1/2 -translate-y-1/2 p-3 bg-black/50 border border-gray-700 rounded-full hover:bg-gray-800 transition-all z-20"
+                    title="Next Chapter"
+                >
+                    <FaChevronRight size={24} />
+                </Link>
+            )}
+
             {/* Header */}
             <header className="sticky top-0 bg-black border-b border-gray-800 p-4 md:p-6 z-10">
                 <div className="max-w-4xl mx-auto flex flex-col items-center gap-4 relative">
